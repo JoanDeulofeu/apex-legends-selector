edbbd4ba2eac6c22772022b27a4d303081615ac3
JoanDeulofeu <joan@outlook.fr>
2021-12-16 08:47:57 +0100

    [func] Legends filtred by options

--- src/components/LegendsPaper.jsx
+++ src/components/LegendsPaper.jsx
@@ -3,12 +3,14 @@ import { createStyles, makeStyles } from "@mui/styles";
 import React from "react";
 import legendsImg from "../constants/legendsImg";
 import { capitalize } from "../utils";
+import { useOptions } from "../contexts/OptionsContextProvider";
 
 const useStyles = makeStyles((theme) =>
 	createStyles({
 		container: {
 			borderRadius: 50,
 			padding: 10,
+			minWidth: "100%",
 		},
 		item: {
 			display: "flex",
@@ -33,33 +35,48 @@ const useStyles = makeStyles((theme) =>
 
 const LegendsPaper = () => {
 	const styles = useStyles();
+	const { banLegends, isSolo } = useOptions();
 
 	return (
-		<Paper className={styles.container}>
-			<Grid container>
-				{Object.keys(legendsImg).map((_legendName) => (
-					<Grid
-						key={_legendName}
-						item
-						xs={12}
-						sm={6}
-						md={4}
-						lg={3}
-						xl={2}
-						className={styles.item}
-					>
-						<img
-							className={styles.legendImg}
-							alt={_legendName}
-							src={legendsImg[_legendName]}
-						></img>
-						<div className={styles.textContainer}>
-							<Typography variant="text">{capitalize(_legendName)}</Typography>
-						</div>
+		<>
+			{banLegends.length < Object.keys(legendsImg).length - (isSolo ? 1 : 2) ? (
+				<Paper className={styles.container}>
+					<Grid container>
+						{Object.keys(legendsImg).map((_legendName) => {
+							if (!banLegends.includes(_legendName))
+								return (
+									<Grid
+										key={_legendName}
+										item
+										xs={12}
+										sm={6}
+										md={4}
+										lg={3}
+										xl={2}
+										className={styles.item}
+									>
+										<img
+											className={styles.legendImg}
+											alt={_legendName}
+											src={legendsImg[_legendName]}
+										></img>
+										<div className={styles.textContainer}>
+											<Typography variant="text">
+												{capitalize(_legendName)}
+											</Typography>
+										</div>
+									</Grid>
+								);
+							else return <></>;
+						})}
 					</Grid>
-				))}
-			</Grid>
-		</Paper>
+				</Paper>
+			) : (
+				<Typography variant="text">
+					{`Please select at least ${isSolo ? "two" : "three"} legends ...`}
+				</Typography>
+			)}
+		</>
 	);
 };
 
