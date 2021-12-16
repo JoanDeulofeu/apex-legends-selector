ae31f11a14837cbb38d58ed388ef06e14c7157bf
JoanDeulofeu <joan@outlook.fr>
2021-12-16 14:10:35 +0100

    [func] Add new utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,15 @@
 const capitalize = (str) => {
-	return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1);
+	if (typeof str !== "string") {
+		return "";
+	} else {
+		return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1);
+	}
 };
 
-export { capitalize };
+const timeOut = (ms) => new Promise((f) => setTimeout(f, ms));
+
+const getRandomInt = (max = 0) => {
+	return Math.floor(Math.random() * max);
+};
+
+export { capitalize, timeOut, getRandomInt };
